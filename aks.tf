5f556b7e759035167f6a56504949c6074d611e5e
Hasan Tutac <tutachasan@gmail.com>
2020-12-06 20:02:51 +0000

    aks azure

---
+++ aks.tf
@@ -0,0 +1,15 @@
+module "aks" {
+        source = "tutac/terraform-azure-aks"
+        cluster_name = "example-aks1"
+        kubernetes_version = "1.18.10"
+        node_pool_name = "node1"
+        min_count = "1"
+        max_count = "1"
+        client_id = "ID"
+        client_secret = "PASSWORD"
+        environment = "dev"
+        resource_group_name_location = "West Europe"
+        resource_group_name = "dev"
+        username = "centos"
+        vm_size = "Standard_A2_v2"
+}
